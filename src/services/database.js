e8f6301b447cb8792894d8a79ba8eb8918dda86b
GonzaBianchi <gonzalobianchini70@gmail.com>
2025-05-20 14:48:53 -0300

    mongodb

--- src/services/database.js
+++ src/services/database.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 export async function connectToDatabase() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ Conectado a MongoDB Atlas');
   } catch (error) {
     console.error('❌ Error conectando a MongoDB:', error);
