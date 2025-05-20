d4baeb63c5a18779c3ac973f0b56dd29a26a80ec
GonzaBianchi <gonzalobianchini70@gmail.com>
2025-05-19 21:08:11 -0300

    conexionbd

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Conexión a MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('✅ Conectado a MongoDB Atlas'))
 .catch(err => console.error('❌ Error conectando a MongoDB:', err));
 
